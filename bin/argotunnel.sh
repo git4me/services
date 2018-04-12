b939e418f4eff75845e56caa5286a9c4d066379f
JingleManSweep <jinglemansweep@gmail.com>
2018-04-12 20:20:59 +0100

    Updates

--- bin/argotunnel.sh
+++ bin/argotunnel.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-killall cloudflared >/dev/null 2>&1
-echo $CNAME
+source ./secrets/env.sh
 
-cloudflared --hostname gitea.$CNAME      http://d1.home:3001  >/dev/null 2>&1 &
-cloudflared --hostname grafana.$CNAME    http://d1.home:3000  >/dev/null 2>&1 &
-cloudflared --hostname motion.$CNAME     http://d1.home:8765  >/dev/null 2>&1 &
-cloudflared --hostname plex.$CNAME       http://d1.home:32400 >/dev/null 2>&1 &
-cloudflared --hostname portainer.$CNAME  http://d1.home:9000  >/dev/null 2>&1 &
-cloudflared --hostname rundeck.$CNAME    http://d1.home:4440  >/dev/null 2>&1 &
+killall cloudflared >/dev/null 2>&1
+
+screen -dmS argo_gitea      cloudflared --hostname gitea.$CNAME      http://d1.home:3001
+screen -dmS argo_grafana    cloudflared --hostname grafana.$CNAME    http://d1.home:3000
+screen -dmS argo_motion     cloudflared --hostname motion.$CNAME     http://d1.home:8765
+screen -dmS argo_plex       cloudflared --hostname plex.$CNAME       http://d1.home:32400
+screen -dmS argo_portainer  cloudflared --hostname portainer.$CNAME  http://d1.home:9000
+screen -dmS argo_rundeck    cloudflared --hostname rundeck.$CNAME    http://d1.home:4440
 
