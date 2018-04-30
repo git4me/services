5efdb7df409f6dfebb64393236bf938c5891f41e
JingleManSweep <jinglemansweep@gmail.com>
2018-04-30 14:39:37 +0100

    Updates

---
+++ bin/compose.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker-compose \
+  -f stacks/gitea.yml \
+  -f stacks/grafana.yml \
+  -f stacks/influxdb.yml \
+  -f stacks/mongodb.yml \
+  -f stacks/mosquitto.yml \
+  -f stacks/motion.yml \
+  -f stacks/mysql.yml \
+  -f stacks/plex.yml \
+  -f stacks/redis.yml \
+  -f stacks/rundeck.yml \
+  -f stacks/traefik.yml \
+  -f stacks/unifi.yml \
+  config
+
